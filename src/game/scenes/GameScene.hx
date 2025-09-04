e11a4b2fbe5c4365cbcd0fecb5108b449f94f80d
zzox <tweedlitweedli@gmail.com>
2025-09-04 10:27:32 -0500

    arraysort tiles

--- src/game/scenes/GameScene.hx
+++ src/game/scenes/GameScene.hx
@@ -2,7 +2,9 @@ package game.scenes;
 
 import core.Game;
 import core.scene.Scene;
+import haxe.ds.ArraySort;
 import kha.Assets;
+import kha.Window;
 import kha.input.KeyCode;
 
 typedef Grid<T> = {
@@ -59,6 +61,21 @@ function forEachGI<T> (grid:Grid<T>, callback:(x:Int, y:Int, item:T) -> Void) {
     }
 }
 
+function mapGIItems<T, TT> (grid:Grid<T>, callback:(x:Int, y:Int, item:T) -> TT):Array<TT> {
+    // don't know about this as it requires a cast
+    // if (callback == null) {
+    //     callback = (x:Int, y:Int, item:T) -> { return cast(item); };
+    // }
+
+    final items = [];
+    for (x in 0...grid.width) {
+        for (y in 0...grid.height) {
+            items.push(callback(x, y, grid.items[x + y * grid.width]));
+        }
+    }
+    return items;
+}
+
 class GameScene extends Scene {
     var grid:Grid<Int>;
     override function create () {
@@ -96,6 +113,10 @@ class GameScene extends Scene {
     override function render (g2, cam) {
         super.render(g2, cam);
 
+        // PERF: only do this on rotation
+        final items = mapGIItems(grid, (x, y, item) -> { return { item: item, x: x, y: y } });
+        ArraySort.sort(items, (a, b) -> Std.int(translateWorldY(a.x, a.y, SouthEast)) - Std.int(translateWorldY(b.x, b.y, SouthEast)));
+
         g2.begin(true, camera.bgColor);
 
         // g2.color = Math.floor(alpha * 256) * 0x1000000 + color;
@@ -104,11 +125,11 @@ class GameScene extends Scene {
         g2.pushTranslation(-camera.scrollX, -camera.scrollY);
         g2.pushScale(camera.scale, camera.scale);
 
-        forEachGI(grid, (x, y, item) -> {
-            g2.drawSubImage(Assets.images.tiles, translateWorldX(x, y, SouthEast), translateWorldY(x, y, SouthEast),
+        for (i in 0...items.length) {
+            g2.drawSubImage(Assets.images.tiles, translateWorldX(items[i].x, items[i].y, SouthEast), translateWorldY(items[i].x, items[i].y, SouthEast),
                 0, 0, 16, 16
             );
-        });
+        }
 
         g2.popTransformation();
         g2.popTransformation();
